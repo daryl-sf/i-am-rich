e107da0f5beef9f88b9f0174fcda12a79afe3e04
Daryl Findlay <darylfindlay@gmail.com>
2019-05-04 19:00:56 +0100

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Daryl Findlay on 04/05/2019.
+//  Copyright © 2019 Daryl Findlay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
